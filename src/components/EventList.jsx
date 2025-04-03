77272201ec51c679071a1e81abdc004c95ce26a3
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2025-04-03 11:02:40 +0200

    integrating the API-v3

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Event from "./Event";
 
-const EventList = ({ events }) => {
+const EventList = ({ events = [] }) => {
   return (
     <ul id="event-list">
-      {events
-        ? events.map((event) => <Event key={event.id} event={event} />)
-        : null}
+      {events.length > 0 ? (
+        events.map((event) => <Event key={event.id} event={event} />)
+      ) : (
+        <li>No events available</li>
+      )}
     </ul>
   );
 };
