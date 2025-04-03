78116b9ce76694d18d733b39374e78d911f1e9e0
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2025-04-03 15:49:04 +0200

    Save changes to EventList.jsx

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -3,11 +3,11 @@ import Event from "./Event";
 
 const EventList = ({ events = [] }) => {
   return (
-    <ul id="event-list">
+    <ul id="event-list" role="list">
       {events.length > 0 ? (
         events.map((event) => <Event key={event.id} event={event} />)
       ) : (
-        <li>No events available</li>
+        <li>No events available</li> // Fallback message
       )}
     </ul>
   );
