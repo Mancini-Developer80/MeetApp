3d8c76a1f70f041e14935202c7efa76ee7da69a7
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2025-04-14 15:27:30 +0200

    serviceWorkerRegistration import

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import * as atatus from "atatus-spa";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 atatus.config("f743440dca674b5aa221bc6c38f023a2").install();
 atatus.notify(new Error("Test Atatus Setup"));
